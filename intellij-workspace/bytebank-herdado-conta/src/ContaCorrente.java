f70a8f7141989ad8e2d31c1b8b07344fcb085947
Matheus <matheusmrf@gmail.com>
2021-01-30 00:50:39 -0300

    aula 4 - Herança e uso de contrutores

--- intellij-workspace/bytebank-herdado-conta/src/ContaCorrente.java
+++ intellij-workspace/bytebank-herdado-conta/src/ContaCorrente.java
@@ -6,4 +6,9 @@ public class ContaCorrente extends Conta {
         super(agencia, numero);
     }
 
+    @Override
+    public boolean saca(double valor) {
+        double valorASacar = valor + 0.2;
+        return super.saca(valorASacar);
+    }
 }
